0c066dafba767bb2071c6816befe3eea4b368d49
KreloX <krelox@seznam.cz>
2024-06-29 14:04:49 +0200

    feat: update to 1.18.2

--- src/main/java/krelox/spartansimpleores/SpartanSimpleOres.java
+++ src/main/java/krelox/spartansimpleores/SpartanSimpleOres.java
@@ -1,20 +1,61 @@
 package krelox.spartansimpleores;
 
-import krelox.spartansimpleores.init.ItemRegistrySSO;
+import krelox.spartantoolkit.SpartanAddon;
+import krelox.spartantoolkit.SpartanMaterial;
+import krelox.spartantoolkit.WeaponMap;
+import krelox.spartantoolkit.WeaponType;
+import mod.alexndr.simplecorelib.api.helpers.TagUtils;
+import mod.alexndr.simpleores.content.SimpleOresTiers;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Mod(SpartanSimpleOres.MODID)
-public class SpartanSimpleOres 
-{
-	public static final String MODID = "spartansimpleores";
-	
-	public SpartanSimpleOres() 
-	{
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		MinecraftForge.EVENT_BUS.register(this);
-		ItemRegistrySSO.ITEMS.register(bus);
-	}
+public class SpartanSimpleOres extends SpartanAddon {
+    public static final String MODID = "spartansimpleores";
+
+    public static final DeferredRegister<Item> ITEMS = itemRegister(MODID);
+    public static final WeaponMap WEAPONS = new WeaponMap();
+
+    public static final SpartanMaterial MYTHRIL = new SpartanMaterial("mythril", MODID, SimpleOresTiers.MYTHRIL, TagUtils.forgeTag("ingots/mythril"), Set.of(), Map.of());
+    public static final SpartanMaterial ADAMANTIUM = new SpartanMaterial("adamantium", MODID, SimpleOresTiers.ADAMANTIUM, TagUtils.forgeTag("ingots/adamantium"), Set.of(), Map.of());
+    public static final SpartanMaterial ONYX = new SpartanMaterial("onyx", MODID, SimpleOresTiers.ONYX, TagUtils.forgeTag("gems/onyx"), Set.of(), Map.of());
+
+    public static final CreativeModeTab SPARTAN_SIMPLEORES_TAB = tab(MODID, () -> WEAPONS.get(MYTHRIL, WeaponType.GREATSWORD).get());
+
+    public SpartanSimpleOres() {
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        registerSpartanWeapons(ITEMS);
+        ITEMS.register(bus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Override
+    public String modid() {
+        return MODID;
+    }
+
+    @Override
+    public List<SpartanMaterial> getMaterials() {
+        return List.of(MYTHRIL, ADAMANTIUM, ONYX);
+    }
+
+    @Override
+    public CreativeModeTab getTab() {
+        return SPARTAN_SIMPLEORES_TAB;
+    }
+
+    @Override
+    public WeaponMap getWeaponMap() {
+        return WEAPONS;
+    }
 }
