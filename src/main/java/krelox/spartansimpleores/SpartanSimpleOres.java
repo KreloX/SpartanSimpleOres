e2bad1649f975e0f21ac83775950a62861dc658c
KreloX <krelox@seznam.cz>
2024-07-09 22:20:48 +0200

    feat: update to 1.20.1

--- src/main/java/krelox/spartansimpleores/SpartanSimpleOres.java
+++ src/main/java/krelox/spartansimpleores/SpartanSimpleOres.java
@@ -1,17 +1,14 @@
 package krelox.spartansimpleores;
 
 import com.oblivioussp.spartanweaponry.api.WeaponMaterial;
-import com.oblivioussp.spartanweaponry.api.trait.RangedCallbackWeaponTrait;
 import com.oblivioussp.spartanweaponry.api.trait.WeaponTrait;
-import krelox.spartantoolkit.SpartanAddon;
-import krelox.spartantoolkit.SpartanMaterial;
-import krelox.spartantoolkit.WeaponMap;
-import krelox.spartantoolkit.WeaponType;
+import krelox.spartantoolkit.*;
 import mod.alexndr.simplecorelib.api.helpers.TagUtils;
 import mod.alexndr.simpleores.content.SimpleOresTiers;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -19,7 +16,6 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Mod(SpartanSimpleOres.MODID)
 public class SpartanSimpleOres extends SpartanAddon {
@@ -28,21 +24,30 @@ public class SpartanSimpleOres extends SpartanAddon {
     public static final WeaponMap WEAPONS = new WeaponMap();
     public static final DeferredRegister<Item> ITEMS = itemRegister(MODID);
     public static final DeferredRegister<WeaponTrait> TRAITS = traitRegister(MODID);
+    public static final DeferredRegister<CreativeModeTab> TABS = tabRegister(MODID);
 
     // Traits
-    public static final RegistryObject<WeaponTrait> FLAME = registerTrait(TRAITS, new RangedCallbackWeaponTrait("flame", SpartanSimpleOres.MODID, WeaponTrait.TraitQuality.POSITIVE) {
+    public static final RegistryObject<WeaponTrait> FLAME = registerTrait(TRAITS, new BetterWeaponTrait("flame", SpartanSimpleOres.MODID, WeaponTrait.TraitQuality.POSITIVE) {
         @Override
         public void onProjectileSpawn(WeaponMaterial material, AbstractArrow projectile) {
             projectile.setSecondsOnFire(100);
         }
+
+        @Override
+        public boolean isEnabled(WeaponMaterial material, ItemStack stack) {
+            return super.isEnabled(material, stack) && ((WeaponItem) stack.getItem()).isRanged();
+        }
     }.setRanged());
 
     // Materials
-    public static final SpartanMaterial MYTHRIL = new SpartanMaterial("mythril", MODID, SimpleOresTiers.MYTHRIL, TagUtils.forgeTag("ingots/mythril"), Set.of(), Map.of());
-    public static final SpartanMaterial ADAMANTIUM = new SpartanMaterial("adamantium", MODID, SimpleOresTiers.ADAMANTIUM, TagUtils.forgeTag("ingots/adamantium"), Set.of(), Map.of());
-    public static final SpartanMaterial ONYX = new SpartanMaterial("onyx", MODID, SimpleOresTiers.ONYX, TagUtils.forgeTag("gems/onyx"), Set.of(), Map.of());
+    public static final SpartanMaterial MYTHRIL = new SpartanMaterial("mythril", MODID, SimpleOresTiers.MYTHRIL, TagUtils.forgeTag("ingots/mythril"));
+    public static final SpartanMaterial ADAMANTIUM = new SpartanMaterial("adamantium", MODID, SimpleOresTiers.ADAMANTIUM, TagUtils.forgeTag("ingots/adamantium"));
+    public static final SpartanMaterial ONYX = new SpartanMaterial("onyx", MODID, SimpleOresTiers.ONYX, TagUtils.forgeTag("gems/onyx"), FLAME);
 
-    public static final CreativeModeTab SPARTAN_SIMPLEORES_TAB = tab(MODID, () -> WEAPONS.get(MYTHRIL, WeaponType.GREATSWORD).get());
+    @SuppressWarnings("unused")
+    public static final RegistryObject<CreativeModeTab> SPARTAN_SIMPLEORES_TAB = registerTab(TABS, MODID,
+            () -> WEAPONS.get(MYTHRIL, WeaponType.GREATSWORD).get(),
+            (parameters, output) -> ITEMS.getEntries().forEach(item -> output.accept(item.get())));
 
     public SpartanSimpleOres() {
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -50,6 +55,7 @@ public class SpartanSimpleOres extends SpartanAddon {
         registerSpartanWeapons(ITEMS);
         ITEMS.register(bus);
         TRAITS.register(bus);
+        TABS.register(bus);
     }
 
     @Override
@@ -65,11 +71,6 @@ public class SpartanSimpleOres extends SpartanAddon {
     @Override
     public List<SpartanMaterial> getMaterials() {
         return List.of(MYTHRIL, ADAMANTIUM, ONYX);
-    }
-
-    @Override
-    public CreativeModeTab getTab() {
-        return SPARTAN_SIMPLEORES_TAB;
     }
 
     @Override
